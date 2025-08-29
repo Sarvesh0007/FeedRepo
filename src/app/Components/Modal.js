3eca14ddcfaee51d93f355ae21c2d8674d0ef664
Sarvesh001 <sarvesh.mishra@hdworks.in>
2025-08-29 19:03:31 +0530

    Modal UI completed

--- src/app/Components/Modal.js
+++ src/app/Components/Modal.js
@@ -1,22 +1,47 @@
+import Image from "next/image";
 import { useEffect, useRef } from "react";
+import LoginIcon from "../../../public/Svgs/LoginIcon";
 
-export default function Modal({ onChange }) {
+export default function Modal({
+  onChange,
+  title,
+  desc,
+  inputObj,
+  submitBtnTxt,
+}) {
   const modalRef = useRef(null);
 
-  useEffect(() => {
-    function handleClickedOutside(event) {
-      if (modalRef.current && !modalRef?.current?.contains(event.target)) {
-        onChange(false);
-      }
-    }
-    document.addEventListener("mousedown", handleClickedOutside);
-    return () =>
-      document.removeEventListener("mousedown", handleClickedOutside);
-  }, [onChange]);
-
   return (
-    <div className="bg-gray-300 p-1 w-56 h-44 rounded-md pb-16" ref={modalRef}>
-      <div className="bg-white rounded-md p-4 w-full h-full">xx</div>
+    <div className="bg-white rounded-3xl p-4 w-full h-full">
+      <div className="flex items-center justify-center mt-7 mb-4">
+        <picture className="bg-[#F8F8F8] flex items-center justify-center w-12 rounded-full h-12">
+          <LoginIcon />
+        </picture>
+      </div>
+      <p className="font-bold text-xl pb-1.5 text-black text-center">{title}</p>
+      <p className="text-sm font-normal text-center text-[#0000007A]">{desc}</p>
+
+      <div className="px-10 mt-14 space-y-4">
+        {inputObj?.map((item) => {
+          return (
+            <div key={item?.key}>
+              <p className="text-black font-semibold text-sm pb-1">
+                {item?.title}
+              </p>
+              <input
+                className="bg-[#F4F4F4] font-normal text-sm outline-none w-full rounded-lg py-3 px-2"
+                placeholder={item?.placeHolderTxt}
+                type={item?.type === "password" ? "password" : "text"}
+              />
+            </div>
+          );
+        })}
+        <div className="bg-[#5057EA] cursor-pointer rounded-xl p-3 mb-12 mt-5">
+          <p className="font-semibold text-sm text-center text-white">
+            {submitBtnTxt || "Sign-up"}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/atlys-home/page.js
+++ src/app/atlys-home/page.js
@@ -1,14 +1,27 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Modal from "../Components/Modal";
 
 export default function HomePagege() {
   const [openModal, setOpenModal] = useState(false);
 
+  const modalRef = useRef(null);
+
   function handlClick() {
     setOpenModal(true);
   }
+
+  useEffect(() => {
+    function handleClickedOutside(event) {
+      if (modalRef.current && !modalRef?.current?.contains(event.target)) {
+        setOpenModal(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickedOutside);
+    return () =>
+      document.removeEventListener("mousedown", handleClickedOutside);
+  }, [openModal]);
 
   return (
     <div className="h-screen w-full flex justify-center items-center bg-white">
@@ -17,9 +30,43 @@ export default function HomePagege() {
       </div>
       {openModal && (
         <div className="fixed flex items-center justify-center backdrop-blur-lg bg-black/30 inset-0">
-          <Modal onChange={setOpenModal} />
+          <div
+            ref={modalRef}
+            className="bg-[#EBEBEB] px-3 pt-3 min-w-[498px] min-h-[572px] rounded-3xl"
+          >
+            <Modal
+              inputObj={inputObj}
+              onChange={setOpenModal}
+              title="Sign in to continue"
+              desc="Sign in to access all the features on this app"
+            />
+            <div className="flex items-center justify-center py-4">
+              <span className="font-semibold text-xs text-[#979797] mr-3">
+                Do you have a account
+              </span>
+              <span className="font-bold text-sm text-[#5057EA]">SignUp</span>
+            </div>
+          </div>
         </div>
       )}
     </div>
   );
 }
+
+const inputObj = [
+  {
+    key: "email_key",
+    title: "Email or username",
+    placeHolderTxt: "Enter your email or username",
+  },
+  {
+    key: "password_key",
+    title: "Password",
+    placeHolderTxt: "Enter your password",
+  },
+  {
+    key: "repeat_password_key",
+    title: "Repeat password",
+    placeHolderTxt: "Enter your password again",
+  },
+];
